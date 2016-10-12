88ad2a7ac2e796799343c6b625e6e84432b17c22
Wirili <332477694@qq.com>
2016-10-12 23:30:21 +0800

    init

---
+++ resources/views/mobile/index.blade.php
@@ -0,0 +1,38 @@
+@extends('mobile.layout')
+
+@section('content')
+<ul class="pl-list">
+    <li><img src="build\mobile\images\goods_head.jpg" alt=""></li>
+    <li><a href="#"><img src="data\images\img\102_2016100523150545419.jpg" alt=""></a></li>
+    <li><a href="#"><img src="data\images\img\102_2016100523150545419.jpg" alt=""></a></li>
+    <li><a href="#"><img src="data\images\img\102_2016100523150545419.jpg" alt=""></a></li>
+    <li><a href="#"><img src="data\images\img\102_2016100523150545419.jpg" alt=""></a></li>
+</ul>
+<ul class="main-nav">
+    <li class="active">
+        <a href="#">
+            <i class="icon-home"></i><span>首页</span>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <i class="icon-shop"></i><span>微馆</span>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <i class="icon-cart"></i><span>购物车</span>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <i class="icon-search"></i><span>搜索</span>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <i class="icon-my"></i><span>我的</span>
+        </a>
+    </li>
+</ul>
+@endsection
---
+++ resources/views/mobile/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>app首页</title>
+
+    <!-- Styles -->
+    <link href="{{asset('build/common/css/bootstrap.min.css')}}" rel="stylesheet" type='text/css'>
+    <link href="{{asset('build/common/css/font-awesome.min.css')}}" rel="stylesheet" type='text/css'>
+
+    <link href="{{ elixir('mobile/css/default.css') }}" rel="stylesheet" type='text/css'>
+
+    <!-- JavaScripts -->
+    <script src="{{asset('build/common/js/jquery-1.12.3.min.js')}}"></script>
+
+    <script src="{{asset('build/common/js/bootstrap.min.js')}}"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+
+    <![endif]-->
+    @yield('header')
+</head>
+<body class="skin-blue">
+@yield('content')
+</body>
+</html>
